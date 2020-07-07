ac85b271aeac7b047d66a1458d99e5ea5dc6c27a
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-07-07 16:55:59 +0300

    smaller corrections

--- task_2.py
+++ task_2.py
@@ -5,8 +5,8 @@ def generator(path):
     with open(path, 'r') as target_file:
         for f in target_file:
             line = target_file.readline()
-            hash_obj = hashlib.md5(line.encode())
-            yield hash_obj
+            hash_object = hashlib.md5(line.encode())
+            yield hash_object
 
 
 if __name__ == '__main__':
